113af0dd6da44ab0240fbbfbef49cfdd13f3510a
Pett97 <henriquepeterson138@gmail.com>
2024-05-14 00:24:18 -0300

    brand controller teste ok

---
+++ tests/Unit/Controllers/BrandControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+//require "/var/www/core/constants/general.php";
+
+use App\Controllers\BrandsController;
+use App\Models\Brand;
+use PHPUnit\Framework\TestCase;
+
+
+class BrandControllerTest extends TestCase
+{
+
+    public function test_list_all_brands()
+    {
+        $brands[] = new Brand(name: "Marca Teste1");
+        $brands[] = new Brand(name: "Marca Teste2");
+
+        foreach ($brands as $brand) {
+            $brand->save();
+        }
+
+        $controller = new BrandsController();
+        ob_start();
+        $controller->index();
+        $response = ob_get_contents();
+        ob_end_clean();
+
+        foreach ($brands as $brand) {
+            $this->assertMatchesRegularExpression("/{$brand->getName()}/", $response);
+        }
+    }
+}
