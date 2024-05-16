5f7e12afef5b7ed98d1c60a3b551300f2f87f38b
Pett97 <henriquepeterson138@gmail.com>
2024-05-15 23:13:05 -0300

    adjuste errosHandler

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . "/../core/constants/general.php";
-
-
+require "/var/www/core/Erros/ErrorsHandler.php";
 require "/var/www/core/env/env.php";
 
-require __DIR__ . '/../vendor/autoload.php';
+use Core\Errors\ErrorsHandler;
+
+
+ErrorsHandler::init();
