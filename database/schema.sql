fb259f4558a9ed6f7952a109fdacad396af1c284
Pett97 <henriquepeterson138@gmail.com>
2024-06-07 22:31:10 -0300

    pdo with schema ok

---
+++ database/schema.sql
@@ -0,0 +1,18 @@
+
+
+use listcar;
+
+drop table if exists cars;
+
+create table cars (
+	id int auto_increment primary key,
+    name varchar(55) not null
+);
+
+drop table if exists brands;
+
+create table brands(
+	id int auto_increment primary key,
+    name varchar(55) not null
+);
+
